3e23d10b31da36dc76891d439734824bc96183f2
Gabriel Souza <rorato91@gmail.com>
2022-06-23 17:33:08 -0300

    Update forca.py

--- forca.py
+++ forca.py
@@ -4,29 +4,36 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-    palavra_secreta = 'python'
-    letras_acertadas = ['_', '_', '_', '_', '_', '_']
+    palavra_secreta = 'python'.upper()
+    letras_acertadas = ['_' for letra in palavra_secreta]
     
     enforcou = False
     acertou = False
+    erros = 0
 
     print(letras_acertadas)
     
     while(not enforcou and not acertou):
-        chute = input('Qual letra?')
-        chute = chute.strip()
+        chute = input('Qual letra? ')
+        chute = chute.strip().upper()
 
         if(chute in palavra_secreta):
             index = 0
             for letra in palavra_secreta:
-                if(chute.upper() == letra.upper()):
+                if(chute == letra):
                     letras_acertadas[index] = letra
                 index += 1
         else:
-            erros = erros + 1
+            erros += 1
 
         enforcou = erros == 6
+        acertou = '_' not in letras_acertadas
         print(letras_acertadas)
+
+    if(acertou):
+        print('Você ganhou!')
+    else:
+        print('Você perdeu!')
         
     print('Fim do jogo')
 
